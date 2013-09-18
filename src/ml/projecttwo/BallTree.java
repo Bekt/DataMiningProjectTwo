aa8a64b04d53b856ba80f28f72eda03e3bd046e8
SawyerAnderson <sawyer.lemon@gmail.com>
2013-09-17 20:33:56 -0500

    Added KNN, needs testing

--- src/ml/projecttwo/BallTree.java
+++ src/ml/projecttwo/BallTree.java
@@ -1,7 +1,11 @@
 package ml.projecttwo;
 
 import ml.Matrix;
+
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class BallTree {
 
@@ -11,7 +15,61 @@ public class BallTree {
         root = BallNode.buildBallNode(points);
     }
 
-    public List<Double> findNeighbors(int k, List<Double> point) {
-        throw new UnsupportedOperationException("Not implemented");
+    public PriorityQueue<List<Double>> findNeighbors(int k, final List<Double> point) {
+        PriorityQueue<List<Double>> points = new PriorityQueue<List<Double>>(10, new Comparator<List<Double>>() {
+            @Override
+            public int compare(List<Double> o1, List<Double> o2) {
+                //find Euclidean distances from point
+                double o1dist = Vector.distance(point, o1);
+                double o2dist = Vector.distance(point, o2);
+
+                //rank in reverse (max to min) order
+                if (o1dist < o2dist) return 1;
+                if (o1dist == o2dist) return 0;
+                return -1;
+            }
+        });
+        PriorityQueue<BallNode> balls = new PriorityQueue<BallNode>(10, new Comparator<BallNode>() {
+            @Override
+            public int compare(BallNode o1, BallNode o2) {
+                double o1dist = o1.distanceTo(point);
+                double o2dist = o2.distanceTo(point);
+
+                //rank in min to max order
+                if (o1dist > o2dist) return 1;
+                if (o1dist == o2dist) return 0;
+                return -1;
+            }
+        });
+
+        balls.add(root);
+
+        boolean cont = true;
+        while (cont){
+            BallNode ball = balls.poll(); //get S
+            if (points.size() >= k){ //if there are at least k points in the points PriorityQueue
+                List<Double> tempPoint = points.poll(); //get the first point, S, in points
+                if (ball.distanceTo(point) > Vector.distance(tempPoint, point)){ //if S is further from d than the first point in N is from d
+                    return points; //return N
+                }
+            }
+            if (ball.isLeaf()){
+                //add each point in S to N
+                for(List<Double> pt : ball.points){
+                    points.add(pt);
+                }
+                while(points.size() > k){
+                    //discard the first point in N
+                    points.poll();
+                }
+            }
+            else{
+                //add each child ball in S to Q
+                balls.add(ball.left);
+                balls.add(ball.right);
+            }
+        }
+
+
     }
 }
