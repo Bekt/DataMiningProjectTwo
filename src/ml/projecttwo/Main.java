a2de50ead2bf249b94169a77f03bdd6318efb0f2
Bekt <bekt17@gmail.com>
2013-09-17 20:41:18 -0500

    Implemented brute force knn

--- src/ml/projecttwo/Main.java
+++ src/ml/projecttwo/Main.java
@@ -1,13 +1,48 @@
 package ml.projecttwo;
 
+import ml.ARFFParser;
+import ml.Matrix;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static ml.projecttwo.Vector.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        Matrix points = ARFFParser.loadARFF("/Users/dev/workspace/DataMiningProjectTwo/brute.arff");
+        List<Double> input = Arrays.asList(70.0, 70.0, 70.0);
+        Set<Integer> out = slowKnn(points, 2, input);
+        for (int i : out) {
+            System.out.println(points.getRow(i));
+        }
     }
 
-    static void slowKnn(int k) {
+    /**
+     * Returns the indecies of k-rows in the matrix
+     */
+    static Set<Integer> slowKnn(Matrix points, int k, List<Double> point) {
+        Set<Integer> indecies = new HashSet<Integer>();
+        for (int i = 0; i < k; i++) {
 
+            double closestDistance = Double.POSITIVE_INFINITY;
+            int closestIndex = -1;
+            for (int j = 0; j < points.getNumRows(); j++) {
+                List<Double> row = points.getRow(j);
+
+                double dist = distance(point, row);
+                if (dist < closestDistance && !indecies.contains(j)) {
+                    closestDistance = dist;
+                    closestIndex = j;
+                }
+            }
+            indecies.add(closestIndex);
+        }
+        return indecies;
     }
 
 }
