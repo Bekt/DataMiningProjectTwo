0cd243c7ef915f8a801258e935b2787984ce6d74
SawyerAnderson <sawyer.lemon@gmail.com>
2013-09-17 20:47:48 -0500

    Changed BallTree KNN to use linear interpolation

--- src/ml/projecttwo/BallTree.java
+++ src/ml/projecttwo/BallTree.java
@@ -50,7 +50,8 @@ public class BallTree {
             BallNode ball = balls.poll(); //get S
             if (points.size() >= k){ //if there are at least k points in the points PriorityQueue
                 List<Double> tempPoint = points.poll(); //get the first point, S, in points
-                if (ball.distanceTo(point) > Vector.distance(tempPoint, point)){ //if S is further from d than the first point in N is from d
+                //if S is further from d than the first point in N is from d (linear interpolation)
+                if ((1/ball.distanceTo(point)) > (1/Vector.distance(tempPoint, point))){
                     return points; //return N
                 }
             }
